bb14b3054b3bc061d7d47a0651b5d69b845bf4fc
jhmt <jun.hashimoto211@gmail.com>
2017-08-19 16:15:34 +1200

    init to heroku deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MdNativeDateModule, MdDatepickerModule, MaterialModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
